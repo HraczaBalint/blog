af1b7150806af51edf6a9f857dbb5083a6d558c1
HraczaBalint <hraczabalintmail@gmail.com>
2022-06-29 23:50:35 +0200

    Layout - yield + section

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MY blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+@extends('layout')
+
+@section('content')
     <article>
         <h1>{{ $post->title }}</h1>
 
@@ -17,5 +10,4 @@
     </article>
 
     <a href="/">Go back</a>
-</body>
-</html>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MY blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-    @foreach ($posts as $post)
-        <article class="{{ $loop->even ? 'foobar' : '' }}">
-            <h1>
-                <a href='/posts/{{ $post->slug }}'>{{ $post->title }}</a>
-            </h1>
+@extends('layout')
 
-            <div>
-                {{ $post->excerpt }}
-            </div>
-        </article>
+@section('banner')
+    <h1>My blog</h1>
+@endsection
+
+@section('content')
+    @foreach ($posts as $post)
+    <article class="{{ $loop->even ? 'foobar' : '' }}">
+        <h1>
+            <a href='/posts/{{ $post->slug }}'>{{ $post->title }}</a>
+        </h1>
+
+        <div>
+            {{ $post->excerpt }}
+        </div>
+    </article>
     @endforeach
-</body>
-</html>
+@endsection
